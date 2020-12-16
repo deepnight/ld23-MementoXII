774a77c2821a3e3489c6e6a526905a4b6eabb41c
Sébastien Bénard <github@deepnight.net>
2020-12-16 15:36:41 +0100

    Added old sources

---
+++ src/Assets.hx
@@ -0,0 +1,54 @@
+class Assets {
+	public static var tiles : SpriteLib;
+
+	public static function init() {
+		var t = hxd.Res.tiles.toTile();
+		tiles = new SpriteLib([t]);
+
+		tiles.setDefaultCenterRatio(0,0);
+
+		tiles.slice("collision",0,  0, 16*2, 8,8);
+		tiles.slice("separator",0,  0,16*3, 16*7,16, 2);
+		tiles.slice("cell",0,  0,16*4, 16*7,16*5, 1, 4);
+
+		tiles.slice("picPart1",0,  7*16,5*16, 16,16);
+		tiles.slice("calendar",0,  7*16,6*16, 16,16);
+		tiles.slice("picPart3",0,  7*16,7*16, 16,16);
+		tiles.slice("framed",0,  7*16,8*16, 16,16);
+		tiles.slice("ringBack",0,  8*16,6*16, 16,16);
+		tiles.slice("reflections",0,  9*16,6*16, 16,16);
+		tiles.sliceGrid("sinkWater",0,  8,5);
+		tiles.sliceAnimGrid("flow",0,2,  8,5, 4); // flow
+
+		tiles.slice("kitchenDoor",0,  7*16,10*16, 16,16);
+		tiles.slice("kitchenKnife",0,  7*16,11*16, 16,16);
+		tiles.slice("kitchenTable",0,  7*16,12*16, 16,16, 2);
+		tiles.slice("kitchenChest",0,  7*16,13*16, 16,16, 2);
+		tiles.slice("cupDoors",0,  8*16,10*16, 16,20);
+
+		tiles.slice("phone",0,  7*16,15*16, 16,16);
+		tiles.slice("finalLetter",0,  7*16,16*16, 16,16);
+
+		tiles.setSliceGrid(16,16);
+		tiles.slice("player",0,  0,0, 16,16, 16,2);
+		tiles.sliceAnimGrid("wakeWait",0,1,  10,0);
+		tiles.sliceAnimGrid("wakeUp",0,1,  10,0, 4);
+		tiles.sliceAnimGrid("standDown",0,1, 0,0);
+		tiles.sliceAnimGrid("standUp",0,1, 5,0);
+
+		tiles.sliceGrid("walkDown",0, 0,0, 5);
+		tiles.defineAnim("0,1,2,1,0, 3,4,3");
+
+		tiles.sliceGrid("walkUp",0, 5,0, 5);
+		tiles.defineAnim("0,1,2,1,0, 3,4,3");
+
+		tiles.sliceAnimGrid("read",0,25,  14,0, 2);
+
+		tiles.sliceGrid("cry",0,  0,1, 5);
+		tiles.defineAnim("0(20), 1(70), 2(10), 3(50), 4(100)");
+		tiles.sliceAnimGrid("afterCry",0,1,  4,1);
+
+		tiles.slice("heroFade",0,  0,16*25, 12*16, 9*16);
+
+	}
+}
---
+++ src/Main.hx
@@ -0,0 +1,78 @@
+import hxd.Key;
+
+class Main extends dn.Process {
+	public static var ME : Main;
+
+	/** Used to create "Access" instances that allow controller checks (keyboard or gamepad) **/
+	public var controller : dn.heaps.Controller;
+
+	/** Controller Access created for Main & Boot **/
+	public var ca : dn.heaps.Controller.ControllerAccess;
+
+	public function new(s:h2d.Scene) {
+		super();
+		ME = this;
+
+        createRoot(s);
+
+		// Engine settings
+		engine.backgroundColor = 0xff<<24|0x111133;
+        #if( hl && !debug )
+        engine.fullScreen = true;
+        #end
+
+		// Heaps resources
+		#if( hl && debug )
+			hxd.Res.initLocal();
+        #else
+      		hxd.Res.initEmbed();
+        #end
+
+		// Assets & data init
+		hxd.snd.Manager.get(); // force sound manager init on startup instead of first sound play
+		Assets.init();
+
+		// Game controller & default key bindings
+		controller = new dn.heaps.Controller(s);
+		ca = controller.createAccess("main");
+		// controller.bind(AXIS_LEFT_X_NEG, Key.LEFT, Key.Q, Key.A);
+		// controller.bind(AXIS_LEFT_X_POS, Key.RIGHT, Key.D);
+		// controller.bind(X, Key.SPACE, Key.F, Key.E);
+		// controller.bind(A, Key.UP, Key.Z, Key.W);
+		// controller.bind(B, Key.ENTER, Key.NUMPAD_ENTER);
+		// controller.bind(SELECT, Key.R);
+		// controller.bind(START, Key.N);
+
+		#if js
+		// Optional helper that shows a "Click to start/continue" message when the game looses focus
+		new dn.heaps.GameFocusHelper(Boot.ME.s2d, Assets.fontMedium);
+		#end
+
+		// Start with 1 frame delay, to avoid 1st frame freezing from the game perspective
+		hxd.Timer.wantedFPS = 30;
+		hxd.Timer.skip();
+		delayer.addF( startGame, 1 );
+		trace("hello");
+	}
+
+	/** Start game process **/
+	public function startGame() {
+		var e = Assets.tiles.h_getAndPlay("flow", root);
+		e.scale(16);
+
+		// if( Game.ME!=null ) {
+		// 	Game.ME.destroy();
+		// 	delayer.addF(function() {
+		// 		new Game();
+		// 	}, 1);
+		// }
+		// else
+		// 	new Game();
+	}
+
+
+    override function update() {
+		Assets.tiles.tmod = tmod;
+		super.update();
+    }
+}
